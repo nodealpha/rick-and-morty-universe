ae25c0c757ab21591e69b6cbec9e1a30214b8b45
Nodealpha <nodealpha.mail@gmail.com>
2021-01-13 14:42:52 +0530

    chore: Removed unneeded imports

--- src/main.js
+++ src/main.js
@@ -5,11 +5,9 @@ import router from './router'
 import store from './store'
 
 import './mixins'
-import './plugins'
 import './thirdParty'
 
 import './scss/style.scss'
-import './assets/fonts/bebasneue.css'
 
 Vue.config.productionTip = false
 
